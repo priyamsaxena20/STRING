92241f4f73534de18621114edb78b140df9d650a
priyamsaxena20 <77077670+priyamsaxena20@users.noreply.github.com>
2023-06-17 16:41:51 +0530

    Create Check_Palindrome.cpp

---
+++ Check_Palindrome.cpp
@@ -0,0 +1,28 @@
+// #include <bits/stdc++.h> 
+// char tolowercase(string str){
+//   if(str>='a'&&str<='z'){
+//       return ch;
+//   }
+//   else{
+//       string temp=str-'A'+'a';
+//       return temp;
+//   }
+
+
+// }
+// bool checkPalindrome(string s)
+// {
+//     // Write your code here.
+//     int st=0;
+//     int e=s.size()-1;
+//     while(s<=e){
+//         if(tolowercase(s[st])!=tolowercase(s[e])){
+//             return 0;
+//         }
+//         else{
+//             s++;
+//             e--;
+//         }
+//     }
+//     return 1;
+// }
