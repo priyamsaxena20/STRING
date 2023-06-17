131a0e97557fe9638d6b5dcc6e38528b57257a0b
priyamsaxena20 <77077670+priyamsaxena20@users.noreply.github.com>
2023-06-17 16:57:16 +0530

    Update Check_Palindrome.cpp

--- Check_Palindrome.cpp
+++ Check_Palindrome.cpp
@@ -26,3 +26,25 @@
 //     }
 //     return 1;
 // }
+
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S) {
+	    // Your code goes here
+	    int left = 0;
+        int right = S.length() - 1;
+
+        while (left < right) {
+            if (S[left] != S[right]) {
+                return 0;            // Not a palindrome
+            }
+            left++;
+            right--;
+        }
+
+        return 1;              // palindrome
+	}
+};        
