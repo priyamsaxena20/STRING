9fa5670a8a75932356f5fd8fb817639e8d002231
priyamsaxena20 <77077670+priyamsaxena20@users.noreply.github.com>
2023-08-21 16:37:36 +0530

    Create Valid_palindrome_2.cpp

---
+++ preparation/Valid_palindrome_2.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkpal(string s,int i,int j){
+        // int i=0;
+        // int j=str.length()-1;
+
+        while(i<=j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+        
+                i++;
+                j--;
+            
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j){
+            if(s[i]!=s[j]){
+                return checkpal(s,i+1,j) ||  checkpal(s,i,j-1);
+            }
+            else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+        
+    }
+};
